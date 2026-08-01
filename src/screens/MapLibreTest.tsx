2e64ff10f4c6939f17101ada3b0b44efb430addc
itsyoungaaron97-netizen <itsyoungaaron97@gmail.com>
2026-08-01 18:38:34 +0200

    Update MapLibreTest.tsx

--- src/screens/MapLibreTest.tsx
+++ src/screens/MapLibreTest.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { MapView, Camera } from "@maplibre/maplibre-react-native";
+import MapLibreGL from "@maplibre/maplibre-react-native";
 
 export default function MapLibreTest() {
   return (
     <View style={styles.container}>
-      <MapView
+      <MapLibreGL.MapView
         style={styles.map}
         styleURL="https://demotiles.maplibre.org/style.json"
       >
-        <Camera
+        <MapLibreGL.Camera
           zoomLevel={14}
           centerCoordinate={[14.2681, 40.8518]}
           pitch={45}
           heading={0}
         />
-      </MapView>
+      </MapLibreGL.MapView>
     </View>
   );
 }
