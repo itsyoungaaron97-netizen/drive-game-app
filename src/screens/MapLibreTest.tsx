6e72d446e2a89e614e8019a90f1120d7a7205dee
itsyoungaaron97-netizen <itsyoungaaron97@gmail.com>
2026-08-02 00:31:16 +0200

    Update MapLibreTest.tsx

--- src/screens/MapLibreTest.tsx
+++ src/screens/MapLibreTest.tsx
@@ -5,26 +5,35 @@ import { Map, Camera } from "@maplibre/maplibre-react-native";
 export default function MapLibreTest() {
   return (
     <View style={styles.container}>
+
       <Map
         style={styles.map}
         mapStyle="https://tiles.openfreemap.org/styles/liberty"
       >
+
         <Camera
-          zoom={14}
-          centerCoordinate={[14.2681, 40.8518]}
-          pitch={45}
-          bearing={0}
+          defaultSettings={{
+            zoomLevel: 14,
+            centerCoordinate: [14.2681, 40.8518],
+            pitch: 45,
+            heading: 0,
+          }}
         />
+
       </Map>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
   },
+
   map: {
     flex: 1,
   },
+
 });
