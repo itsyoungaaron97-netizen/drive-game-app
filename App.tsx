b74aa93d4d92707234dec9dbc4a52e1a1af746ce
itsyoungaaron97-netizen <itsyoungaaron97@gmail.com>
2026-08-02 19:31:02 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,51 +1,173 @@
 import React, { useEffect, useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { View, ActivityIndicator, StyleSheet } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { User } from "firebase/auth";
 
-import MapLibreGL from "@maplibre/maplibre-react-native";
+import {
+  StatusBar,
+} from "expo-status-bar";
 
-import { subscribeToAuth } from "./src/services/firebase";
+import {
+  View,
+  ActivityIndicator,
+  StyleSheet,
+} from "react-native";
+
+import {
+  SafeAreaProvider,
+} from "react-native-safe-area-context";
+
+import {
+  User,
+} from "firebase/auth";
+
+import {
+  subscribeToAuth,
+} from "./src/services/firebase";
+
 import AuthScreen from "./src/screens/AuthScreen";
-import AppNavigator from "./src/navigation/AppNavigator";
-import { colors } from "./src/constants/theme";
 
-MapLibreGL.setAccessToken(null);
+import AppNavigator from "./src/navigation/AppNavigator";
+
+import {
+  colors,
+} from "./src/constants/theme";
+
+
+// =====================================
+// APP START
+// =====================================
+
 
 export default function App() {
-  const [user, setUser] = useState<User | null>(null);
-  const [initializing, setInitializing] = useState(true);
+
+
+  const [
+    user,
+    setUser
+  ] = useState<User | null>(null);
+
+
+
+  const [
+    initializing,
+    setInitializing
+  ] = useState(true);
+
+
 
   useEffect(() => {
-    const unsub = subscribeToAuth((u) => {
-      setUser(u);
-      setInitializing(false);
-    });
-    return unsub;
+
+
+    const unsubscribe =
+      subscribeToAuth(
+        (currentUser) => {
+
+
+          setUser(currentUser);
+
+
+          setInitializing(false);
+
+
+        }
+      );
+
+
+
+    return unsubscribe;
+
+
   }, []);
 
+
+
+
   if (initializing) {
+
+
     return (
+
       <View style={styles.boot}>
-        <ActivityIndicator size="large" color={colors.primary} />
+
+
+        <ActivityIndicator
+
+          size="large"
+
+          color={colors.primary}
+
+        />
+
+
       </View>
+
     );
+
+
   }
 
+
+
+
   return (
+
     <SafeAreaProvider>
-      <StatusBar style="light" />
-      {user ? <AppNavigator /> : <AuthScreen />}
+
+
+      <StatusBar
+
+        style="light"
+
+      />
+
+
+
+      {
+
+        user
+
+        ?
+
+        <AppNavigator />
+
+        :
+
+        <AuthScreen />
+
+      }
+
+
+
     </SafeAreaProvider>
+
   );
+
+
 }
 
+
+
+
 const styles = StyleSheet.create({
-  boot: {
-    flex: 1,
-    backgroundColor: colors.background,
-    justifyContent: "center",
-    alignItems: "center",
+
+
+  boot:{
+
+
+    flex:1,
+
+
+    backgroundColor:
+    colors.background,
+
+
+    justifyContent:
+    "center",
+
+
+    alignItems:
+    "center",
+
+
   },
+
+
 });
