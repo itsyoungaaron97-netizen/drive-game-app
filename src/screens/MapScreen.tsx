4e4310d6409cea23c02a200a6a292c4388be4351
itsyoungaaron97-netizen <itsyoungaaron97@gmail.com>
2026-08-02 17:29:25 +0200

    Update MapScreen.tsx

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -17,7 +17,7 @@ import {
   Animated,
 } from "react-native";
 
-import * as MapLibreGL from 
+import MapLibreGL from "@maplibre/maplibre-react-native";
 
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
