d39cb80bee979299afabb9ba1a7eb1a63b8dab21
itsyoungaaron97-netizen <itsyoungaaron97@gmail.com>
2026-08-02 03:46:27 +0200

    Update MapScreen.tsx

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -17,6 +17,7 @@ import {
 } from "react-native";
 
 import { WebView } from "react-native-webview";
+import MapLibreGL from "@maplibre/maplibre-react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import {
